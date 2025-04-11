75d847668a263727fa93a3edb30f2f7024b59e67
Martyna <167070778+MartynaSzymanskaGitHub@users.noreply.github.com>
2025-04-11 10:14:50 +0200

    Update User components/ Navbar

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,14 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
-import MainPage from "./components/MainPage.tsx";
+import MainPage from "./components/MainPage";
+import Navbar from "./components/Navbar";
 
-function App() {
+const AppLayout = () => {
+    const location = useLocation();
+    const hideNavbarPaths = ["/", "/register"];
+
+    const shouldHideNavbar = hideNavbarPaths.includes(location.pathname);
+
     return (
-        <Router>
+        <>
+            {!shouldHideNavbar && <Navbar />}
             <Routes>
                 <Route path="/" element={<LoginForm />} />
                 <Route path="/mainPage" element={<MainPage />} />
             </Routes>
+        </>
+    );
+};
+
+function App() {
+    return (
+        <Router>
+            <AppLayout />
         </Router>
     );
 }
--- frontend/vite-project/src/components/MainPage.tsx
+++ frontend/vite-project/src/components/MainPage.tsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import "../css/mainPage.css"
 
 type UserDto = {
     username: string;
@@ -49,29 +50,23 @@ const MainPage = () => {
     }, []);
 
     if (!user || error) {
-        console.log("User not logged");
-        return <div>User not logged
-            <button
-            onClick={handleLogout}
-            style={{marginTop: "20px", padding: "10px 20px"}}
-        >
-            Wyloguj się
-        </button></div>
+        return (
+            <div className="main-container">
+                <p className="error-message">Użytkownik niezalogowany</p>
+                <button className="logout-button" onClick={handleLogout}>
+                    Wyloguj się
+                </button>
+            </div>
+        );
     }
 
     return (
-        <div style={{padding: "20px"}}>
+        <div className="main-container">
             <h2>Witaj, {user.name}!</h2>
             <p><strong>Nazwa użytkownika:</strong> {user.username}</p>
             <p><strong>Email:</strong> {user.email}</p>
             <p><strong>Data utworzenia:</strong> {user.creationDate}</p>
             <p><strong>Role:</strong> {user.roles.join(", ")}</p>
-            <button
-                onClick={handleLogout}
-                style={{marginTop: "20px", padding: "10px 20px"}}
-            >
-                Wyloguj się
-            </button>
         </div>
     );
 
