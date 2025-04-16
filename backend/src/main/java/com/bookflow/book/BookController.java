8d7a27c5c957797e93e24e9dcdb30aa23e0788e0
Martyna <167070778+MartynaSzymanskaGitHub@users.noreply.github.com>
2025-04-16 23:45:46 +0200

    Add pagination to BookPage

--- backend/src/main/java/com/bookflow/book/BookController.java
+++ backend/src/main/java/com/bookflow/book/BookController.java
@@ -1,12 +1,16 @@
 package com.bookflow.book;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/book")
@@ -15,8 +19,13 @@ public class BookController {
     private final BookRepository bookRepository;
 
     @GetMapping("/all")
-    public ResponseEntity<?> getAllBook() {
-        List<BookDto> books = bookRepository.findAll().stream().map(book -> BookDto.builder()
+    public ResponseEntity<?> getAllBook(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        Page<Book> pageResult = bookRepository.findAll(PageRequest.of(page,size));
+
+        List<BookDto> books = pageResult.getContent().stream().map(book -> BookDto.builder()
                 .book_id(book.getBook_id())
                 .title(book.getTitle())
                 .yearRelease(book.getYearRelease())
@@ -29,7 +38,14 @@ public class BookController {
                 .availableCopies(book.getAvailableCopies())
                 .totalCopies(book.getTotalCopies())
                 .build()).toList();
-        return ResponseEntity.ok(books);
+        return ResponseEntity.ok(
+                Map.of(
+                    "content", books,
+                    "currentPage", pageResult.getNumber(),
+                    "totalPages", pageResult.getTotalPages(),
+                    "totalElements", pageResult.getTotalElements()
+                )
+        );
     }
 
 
--- frontend/vite-project/src/components/BookPage.tsx
+++ frontend/vite-project/src/components/BookPage.tsx
@@ -1,10 +1,10 @@
-import {useEffect, useState} from "react";
-import {fetchWithRefresh} from "../utils/fetchWithRefresh.tsx";
-import "../css/BookPage.css"
+import { useEffect, useState } from "react";
+import { fetchWithRefresh } from "../utils/fetchWithRefresh.tsx";
+import "../css/BookPage.css";
 
 class Authors {
     name: string | undefined;
-    info : string | undefined;
+    info: string | undefined;
 }
 
 class Category {
@@ -20,52 +20,65 @@ interface BookDto {
     pageCount: number;
     description: string;
     authors: Authors[];
-    categories : Category[];
+    categories: Category[];
     totalCopies: number;
     availableCopies: number;
 }
 
-const BookPage = () => {
+interface BookPageResponse {
+    content: BookDto[];
+    currentPage: number;
+    totalPages: number;
+}
 
-    const [bookList, setBookList] = useState<BookDto[] | null>(null);
+const BookPage = () => {
+    const [bookList, setBookList] = useState<BookDto[]>([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const pageSize = 5;
+
+    const fetchBooks = async (page: number) => {
+        try {
+            const res = await fetchWithRefresh(`http://localhost:8080/book/all?page=${page}&size=${pageSize}`, {
+                method: "GET",
+                credentials: "include"
+            });
+
+            if (!res.ok) {
+                throw new Error("Błąd serwera: " + res.status);
+            }
+
+            const data: BookPageResponse = await res.json();
+            setBookList(data.content);
+            setCurrentPage(data.currentPage);
+            setTotalPages(data.totalPages);
+        } catch (err) {
+            console.error("Błąd pobierania danych:", err);
+        }
+    };
 
     useEffect(() => {
-        fetchWithRefresh("http://localhost:8080/book/all", {
-            method: "GET",
-            credentials: "include"
-        })
-            .then(async res => {
-                if (!res.ok) {
-                    throw new Error("Błąd serwera: " + res.status);
-                }
-                return res.json();
-            })
-            .then((bookData: BookDto[]) => {
-                setBookList(bookData);
-            })
-            .catch(err => console.error("Błąd pobierania danych:", err));
-    }, []);
+        fetchBooks(currentPage);
+    }, [currentPage]);
 
     return (
         <div className="book-wrapper">
             <h2>Lista książek</h2>
-            {bookList ? (
+            {bookList.length > 0 ? (
                 <ul>
                     {bookList.map((book) => (
                         <div className="book-div" key={book.book_id}>
                             <div className="book-image">
                                 <h3><b>{book.title}</b></h3>
-                                <img src={book.jpg} alt={book.title}/>
+                                <img src={book.jpg} alt={book.title} />
                             </div>
                             <div className="book-details">
                                 <p><strong>Opis:</strong> {book.description}</p>
                                 <p><strong>Autorzy:</strong> {book.authors.map(a => a.name).join(", ")}</p>
                                 <p><strong>Kategorie:</strong> {book.categories.map(c => c.category_name).join(", ")}</p>
-                                <p><strong>Ilosc kopii: </strong>{book.totalCopies}</p>
-                                <p><strong>Dostepne: </strong>{book.availableCopies}</p>
-                                <button onClick={() => window.location.href = `/book/${book.book_id}`}>Zobacz
-                                    więcej
-                                </button>
+                                <p><strong>Ilość kopii:</strong> {book.totalCopies}</p>
+                                <p><strong>Dostępne:</strong> {book.availableCopies}</p>
+                                <button onClick={() => window.location.href = `/book/${book.book_id}`}>Zobacz więcej</button>
                             </div>
                         </div>
                     ))}
@@ -73,8 +86,20 @@ const BookPage = () => {
             ) : (
                 <p>Ładowanie danych...</p>
             )}
+
+            <div className="pagination">
+                {Array.from({ length: totalPages }, (_, i) => (
+                    <button
+                        key={i}
+                        onClick={() => setCurrentPage(i)}
+                        className={i === currentPage ? "active-page" : ""}
+                    >
+                        {i + 1}
+                    </button>
+                ))}
+            </div>
         </div>
     );
-
 };
+
 export default BookPage;
