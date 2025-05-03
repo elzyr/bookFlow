12353ddc05d3d3fd4d4f07604cee20361ac020a7
Kamil <elzyr11@gmail.com>
2025-05-03 22:23:42 +0200

    impl of category

--- backend/src/main/java/com/bookflow/category/Category.java
+++ backend/src/main/java/com/bookflow/category/Category.java
@@ -3,25 +3,24 @@ package com.bookflow.category;
 import com.bookflow.book.Book;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.util.List;
 
-@Table(name = "category")
-@AllArgsConstructor
 @Entity
+@Table(name = "category")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
     private Long categoryId;
 
-    @Column(nullable = false, unique = true)
-    private String category_name;
+    @Column(name = "category_name", nullable = false, unique = true)
+    private String categoryName;
 
     @JsonIgnore
     @ManyToMany
@@ -31,8 +30,4 @@ public class Category {
             inverseJoinColumns = @JoinColumn(name = "book_id")
     )
     private List<Book> books;
-
-    public Category() {
-
-    }
 }
