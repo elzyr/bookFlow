3611dd091c024164e935bba73903f61a41e706c3
Kamil <elzyr11@gmail.com>
2025-05-13 18:23:26 +0200

    Update BookDto.tsx

--- frontend/vite-project/src/types/BookDto.tsx
+++ frontend/vite-project/src/types/BookDto.tsx
@@ -2,7 +2,6 @@ import { CategoryInputDto } from "./CategoryInputDto";
 import { Author } from "./Author";
 
 export interface BookDto {
-  /** id zwrócone przez API jako book_id */
   bookId: number;
   title: string;
   yearRelease: number;
@@ -10,9 +9,7 @@ export interface BookDto {
   jpg: string;
   pageCount: number;
   description: string;
-  /** Zwracane przez API w polu authors */
   authors: Author[];
-  /** Zwracane przez API w polu categories */
   categories: CategoryInputDto[];
   totalCopies: number;
   availableCopies: number;
