7b9d8968528c30061e2a0da8318b6b056fb1541b
Kamil <elzyr11@gmail.com>
2025-05-13 18:30:38 +0200

    remove obsolete repositrory queries

--- backend/src/main/java/com/bookflow/loan/LoanRepository.java
+++ backend/src/main/java/com/bookflow/loan/LoanRepository.java
@@ -11,19 +11,10 @@ import java.util.Optional;
 @Repository
 public interface LoanRepository extends JpaRepository<LoanHistory, Long> {
 
-    boolean existsByBook_IdAndUser_UsernameAndReturnedFalse(Long bookId, String username);
-
-    Optional<LoanHistory> findFirstByUser_UsernameAndReturnedFalseAndReturnDateBefore(String username, LocalDate date);
-
-    long countByUser_UsernameAndReturnedFalse(String username);
 
     List<LoanHistory> findByUser_UsernameAndReturnedFalse(String username);
 
     List<LoanHistory> findByUser_UsernameAndReturnedTrue(String username);
-
-    boolean existsByBook_IdAndUser_UsernameAndExtendedTimeTrueAndReturnedFalse(Long bookId, String username);
-
-    List<LoanHistory> findByUser_UsernameAndBook_Id(String username, Long bookId);
 
 
     @Query("SELECT new com.bookflow.loan.BookLoanRankDto(l.book.title, COUNT(l)) " +
