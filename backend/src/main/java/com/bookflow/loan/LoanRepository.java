89b3dcd72d6370e5753f7a8de27e3d79f6609c40
Kamil <elzyr11@gmail.com>
2025-05-29 21:14:09 +0200

    optimize impoorts

--- backend/src/main/java/com/bookflow/loan/LoanRepository.java
+++ backend/src/main/java/com/bookflow/loan/LoanRepository.java
@@ -1,9 +1,6 @@
 package com.bookflow.loan;
 
-import com.bookflow.book.Book;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
